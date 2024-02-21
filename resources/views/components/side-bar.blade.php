6ee9540b5d47dcb66d704e54f0b680d9bf9d1cae
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2024-02-20 22:22:31 -0300

    add original template material design

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,14 +1,15 @@
 <aside class="mdc-drawer mdc-drawer--dismissible mdc-drawer--open">
     <div class="mdc-drawer__header">
-      <a href="index.html" class="brand-logo">
+      {{-- <a href="index.html" class="brand-logo">
         <img src="../assets/images/logo.svg" alt="logo">
-      </a>
+      </a> --}}
+      <h2 class="brand-logo text-white">Tilevu</h2>
     </div>
     <div class="mdc-drawer__content">
-      <div class="user-info">
+      {{-- <div class="user-info">
         <p class="name">Sidney Miranda</p>
         <p class="email">sidney.miranda2013@gmail.com</p>
-      </div>
+      </div> --}}
       <div class="mdc-list-group">
         <nav class="mdc-list mdc-drawer-menu">
           <div class="mdc-list-item mdc-drawer-item">
@@ -20,25 +21,25 @@
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-drawer-link" href="pages/forms/basic-forms.html">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">track_changes</i>
-              Forms
+              PDV
             </a>
           </div>
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="ui-sub-menu">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">dashboard</i>
-              UI Features
+              Cadastro
               <i class="mdc-drawer-arrow material-icons">chevron_right</i>
             </a>
             <div class="mdc-expansion-panel" id="ui-sub-menu">
               <nav class="mdc-list mdc-drawer-submenu">
                 <div class="mdc-list-item mdc-drawer-item">
                   <a class="mdc-drawer-link" href="pages/ui-features/buttons.html">
-                    Buttons
+                    Categorias
                   </a>
                 </div>
                 <div class="mdc-list-item mdc-drawer-item">
                   <a class="mdc-drawer-link" href="pages/ui-features/typography.html">
-                    Typography
+                    Produtos
                   </a>
                 </div>
               </nav>
@@ -47,56 +48,36 @@
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-drawer-link" href="pages/tables/basic-tables.html">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">grid_on</i>
-              Tables
+              Loja
             </a>
           </div>
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-drawer-link" href="pages/charts/chartjs.html">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">pie_chart_outlined</i>
-              Charts
+              Estatisticas
             </a>
           </div>
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="sample-page-submenu">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">pages</i>
-              Sample Pages
+              Cupons
               <i class="mdc-drawer-arrow material-icons">chevron_right</i>
             </a>
             <div class="mdc-expansion-panel" id="sample-page-submenu">
               <nav class="mdc-list mdc-drawer-submenu">
                 <div class="mdc-list-item mdc-drawer-item">
                   <a class="mdc-drawer-link" href="pages/samples/blank-page.html">
-                    Blank Page
+                    Nova campanha
                   </a>
                 </div>
                 <div class="mdc-list-item mdc-drawer-item">
                   <a class="mdc-drawer-link" href="pages/samples/403.html">
-                    403
+                    Ver cupons
                   </a>
                 </div>
                 <div class="mdc-list-item mdc-drawer-item">
                   <a class="mdc-drawer-link" href="pages/samples/404.html">
-                    404
-                  </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                  <a class="mdc-drawer-link" href="pages/samples/500.html">
-                    500
-                  </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                  <a class="mdc-drawer-link" href="pages/samples/505.html">
-                    505
-                  </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                  <a class="mdc-drawer-link" href="pages/samples/login.html">
-                    Login
-                  </a>
-                </div>
-                <div class="mdc-list-item mdc-drawer-item">
-                  <a class="mdc-drawer-link" href="pages/samples/register.html">
-                    Register
+                    Ver campanhas
                   </a>
                 </div>
               </nav>
@@ -105,15 +86,15 @@
           <div class="mdc-list-item mdc-drawer-item">
             <a class="mdc-drawer-link" href="https://www.bootstrapdash.com/demo/material-admin-free/jquery/documentation/documentation.html" target="_blank">
               <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">description</i>
-              Documentation
+              Tutoriais
             </a>
           </div>
         </nav>
       </div>
       <div class="profile-actions">
-        <a href="javascript:;">Settings</a>
+        <a href="javascript:;">Configurações</a>
         <span class="divider"></span>
-        <a href="javascript:;">Logout</a>
+        <a href="javascript:;">Sair</a>
       </div>
       <div class="mdc-card premium-card">
         <div class="d-flex align-items-center">
@@ -121,14 +102,14 @@
             <i class="mdi mdi-shield-outline"></i>
           </div>
           <div>
-            <p class="mt-0 mb-1 ml-2 font-weight-bold tx-12">Material Dash</p>
+            <p class="mt-0 mb-1 ml-2 font-weight-bold tx-12">Tilevu Pro</p>
             <p class="mt-0 mb-0 ml-2 tx-10">Pro available</p>
           </div>
         </div>
-        <p class="tx-8 mt-3 mb-1">More elements. More Pages.</p>
-        <p class="tx-8 mb-3">Starting from $25.</p>
+        <p class="tx-8 mt-3 mb-1">Mais recursos, suporte 24/7</p>
+        <p class="tx-8 mb-3">Pro Light R$49</p>
         <a href="https://www.bootstrapdash.com/product/material-design-admin-template/" target="_blank">
-                      <span class="mdc-button mdc-button--raised mdc-button--white">Upgrade to Pro</span>
+                      <span class="mdc-button mdc-button--raised mdc-button--white">Atualizar para Pro</span>
                   </a>
       </div>
     </div>
--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -2,82 +2,33 @@
     <div class="mdc-top-app-bar__row">
     <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
         <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button sidebar-toggler">menu</button>
-        <span class="mdc-top-app-bar__title">Greetings Clyde!</span>
+        <span class="mdc-top-app-bar__title">Bem Vindo!</span>
         <div class="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon search-text-field d-none d-md-flex">
         <i class="material-icons mdc-text-field__icon">search</i>
         <input class="mdc-text-field__input" id="text-field-hero-input">
         <div class="mdc-notched-outline">
             <div class="mdc-notched-outline__leading"></div>
             <div class="mdc-notched-outline__notch">
-            <label for="text-field-hero-input" class="mdc-floating-label">Search..</label>
+            <label for="text-field-hero-input" class="mdc-floating-label">Buscar..</label>
             </div>
             <div class="mdc-notched-outline__trailing"></div>
         </div>
         </div>
     </div>
     <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-top-app-bar__section-right">
-        <div class="menu-button-container menu-profile d-none d-md-block">
-        <button class="mdc-button mdc-menu-button">
-            <span class="d-flex align-items-center">
-            <span class="figure">
-                <img src="../assets/images/faces/face1.jpg" alt="user" class="user">
-            </span>
-            <span class="user-name">Clyde Miles</span>
-            </span>
-        </button>
-        <div class="mdc-menu mdc-menu-surface" tabindex="-1">
-            <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-account-edit-outline text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Edit profile</h6>
-                </div>
-            </li>
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-settings-outline text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Logout</h6>
-                </div>
-            </li>
-            </ul>
-        </div>
-        </div>
+
         <div class="divider d-none d-md-block"></div>
         <div class="menu-button-container d-none d-md-block">
         <button class="mdc-button mdc-menu-button">
             <i class="mdi mdi-settings"></i>
         </button>
-        <div class="mdc-menu mdc-menu-surface" tabindex="-1">
-            <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-alert-circle-outline text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Settings</h6>
-                </div>
-            </li>
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-progress-download text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Update</h6>
-                </div>
-            </li>
-            </ul>
-        </div>
         </div>
         <div class="menu-button-container">
         <button class="mdc-button mdc-menu-button">
             <i class="mdi mdi-bell"></i>
         </button>
         <div class="mdc-menu mdc-menu-surface" tabindex="-1">
-            <h6 class="title"> <i class="mdi mdi-bell-outline mr-2 tx-16"></i> Notifications</h6>
+            <h6 class="title"> <i class="mdi mdi-bell-outline mr-2 tx-16"></i> Notificações</h6>
             <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
             <li class="mdc-list-item" role="menuitem">
                 <div class="item-thumbnail item-thumbnail-icon">
@@ -118,7 +69,7 @@
             </ul>
         </div>
         </div>
-        <div class="menu-button-container">
+        {{-- <div class="menu-button-container">
         <button class="mdc-button mdc-menu-button">
             <i class="mdi mdi-email"></i>
             <span class="count-indicator">
@@ -126,7 +77,7 @@
             </span>
         </button>
         <div class="mdc-menu mdc-menu-surface" tabindex="-1">
-            <h6 class="title"><i class="mdi mdi-email-outline mr-2 tx-16"></i> Messages</h6>
+            <h6 class="title"><i class="mdi mdi-email-outline mr-2 tx-16"></i> Mensagens</h6>
             <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
             <li class="mdc-list-item" role="menuitem">
                 <div class="item-thumbnail">
@@ -157,31 +108,52 @@
             </li>
             </ul>
         </div>
-        </div>
-        <div class="menu-button-container d-none d-md-block">
-        <button class="mdc-button mdc-menu-button">
-            <i class="mdi mdi-arrow-down-bold-box"></i>
-        </button>
-        <div class="mdc-menu mdc-menu-surface" tabindex="-1">
-            <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-lock-outline text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Lock screen</h6>
-                </div>
-            </li>
-            <li class="mdc-list-item" role="menuitem">
-                <div class="item-thumbnail item-thumbnail-icon-only">
-                <i class="mdi mdi-logout-variant text-primary"></i>
-                </div>
-                <div class="item-content d-flex align-items-start flex-column justify-content-center">
-                <h6 class="item-subject font-weight-normal">Logout</h6>
-                </div>
-            </li>
-            </ul>
-        </div>
+        </div> --}}
+
+        <div class="menu-button-container menu-profile d-none d-md-block">
+            <button class="mdc-button mdc-menu-button">
+                <span class="d-flex align-items-center">
+                <span class="figure">
+                    <img src="../assets/images/faces/face1.jpg" alt="user" class="user">
+                </span>
+                </span>
+            </button>
+            <div class="mdc-menu mdc-menu-surface" tabindex="-1">
+                <ul class="mdc-list" role="menu" aria-hidden="true" aria-orientation="vertical">
+                <li class="mdc-list-item" role="menuitem">
+                    <div class="item-thumbnail item-thumbnail-icon-only">
+                    <i class="mdi mdi-account-edit-outline text-primary"></i>
+                    </div>
+                    <div class="item-content d-flex align-items-start flex-column justify-content-center">
+                    <h6 class="item-subject font-weight-normal">Editar perfil</h6>
+                    </div>
+                </li>
+                <li class="mdc-list-item" role="menuitem">
+                    <div class="item-thumbnail item-thumbnail-icon-only">
+                    <i class="mdi mdi-settings-outline text-primary"></i>
+                    </div>
+                    <div class="item-content d-flex align-items-start flex-column justify-content-center">
+                    <h6 class="item-subject font-weight-normal">Editar loja</h6>
+                    </div>
+                </li>
+                <li class="mdc-list-item" role="menuitem">
+                    <div class="item-thumbnail item-thumbnail-icon-only">
+                    <i class="mdi mdi-settings-outline text-primary"></i>
+                    </div>
+                    <div class="item-content d-flex align-items-start flex-column justify-content-center">
+                    <h6 class="item-subject font-weight-normal">Histórico</h6>
+                    </div>
+                </li>
+                <li class="mdc-list-item" role="menuitem">
+                    <div class="item-thumbnail item-thumbnail-icon-only">
+                    <i class="mdi mdi-logout-variant text-primary"></i>
+                    </div>
+                    <div class="item-content d-flex align-items-start flex-column justify-content-center">
+                    <h6 class="item-subject font-weight-normal">Sair</h6>
+                    </div>
+                </li>
+                </ul>
+            </div>
         </div>
     </div>
     </div>
