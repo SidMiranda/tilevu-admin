43c0bc9f799abf796f9a894c242bbb014a089cbb
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2024-02-22 23:07:03 -0300

    add new template admin pro

---
+++ resources/views/components/side-bar-pro.blade.php
@@ -0,0 +1,300 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item user-info">
+        <p class="name">Clyde Miles</p>
+        <p class="email">clydemiles@elenor.us</p>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-cube menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/samples/widgets.html">
+          <i class="mdi mdi-widgets menu-icon"></i>
+          <span class="menu-title">Widgets</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+          <span class="menu-title">Basic UI Elements</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/accordions.html">Accordions</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/badges.html">Badges</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/breadcrumbs.html">Breadcrumbs</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/modals.html">Modals</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/progress.html">Progress bar</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/pagination.html">Pagination</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/tabs.html">Tabs</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/tooltips.html">Tooltip</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-advanced" aria-expanded="false" aria-controls="ui-advanced">
+          <i class="mdi mdi-cards-variant menu-icon"></i>
+          <span class="menu-title">Advanced UI</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-advanced">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dragula.html">Dragula</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/clipboard.html">Clipboard</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/context-menu.html">Context menu</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/slider.html">Slider</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/colcade.html">Colcade</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/carousel.html">Carousel</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/loaders.html">Loaders</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/treeview.html">Tree View</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/ui-features/popups.html">
+          <i class="mdi mdi-forum menu-icon"></i>
+          <span class="menu-title">Popups</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/ui-features/notifications.html">
+          <i class="mdi mdi-bell-ring menu-icon"></i>
+          <span class="menu-title">Notifications</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+          <i class="mdi mdi-contacts menu-icon"></i>
+          <span class="menu-title">Icons</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="icons">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Material</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/icons/flag-icons.html">Flag icons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/icons/font-awesome.html">Font Awesome</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/icons/simple-line-icon.html">Simple line icons</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/icons/themify.html">Themify icons</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#forms" aria-expanded="false" aria-controls="forms">
+          <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+          <span class="menu-title">Forms</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="forms">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/forms/basic_elements.html">Form Elements</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/forms/advanced_elements.html">Advanced Forms</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/forms/validation.html">Validation</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/forms/wizard.html">Wizard</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/forms/text_editor.html">
+          <i class="mdi mdi-file-document menu-icon"></i>
+          <span class="menu-title">Text editors</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/forms/code_editor.html">
+          <i class="mdi mdi-code-not-equal-variant menu-icon"></i>
+          <span class="menu-title">Code editors</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+          <i class="mdi mdi-chart-bar menu-icon"></i>
+          <span class="menu-title">Charts</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="charts">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/morris.html">Morris</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/flot-chart.html">Flot</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/google-charts.html">Google charts</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/sparkline.html">Sparkline js</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/c3.html">C3 charts</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/chartist.html">Chartist</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/charts/justGage.html">JustGage</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+          <i class="mdi mdi-table-large menu-icon"></i>
+          <span class="menu-title">Tables</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="tables">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/tables/data-table.html">Data table</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/tables/js-grid.html">Js-grid</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/tables/sortable-table.html">Sortable table</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#maps" aria-expanded="false" aria-controls="maps">
+          <i class="mdi mdi-map-marker-radius menu-icon"></i>
+          <span class="menu-title">Maps</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="maps">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/maps/google-maps.html">Google Maps</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/maps/mapael.html">Mapael</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/maps/vector-map.html">Vector map</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+          <i class="mdi mdi-lock menu-icon"></i>
+          <span class="menu-title">User Pages</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="auth">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/mutli-level-login.html"> Multi step login </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
+          <i class="mdi mdi-security menu-icon"></i>
+          <span class="menu-title">Error pages</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="error">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
+          <i class="mdi mdi-medical-bag menu-icon"></i>
+          <span class="menu-title">General Pages</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="general-pages">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/profile.html"> Profile </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/faq.html"> FAQ </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/faq-2.html"> FAQ 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/news-grid.html"> News grid </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/timeline.html"> Timeline </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/user-listing.html"> User listing </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/portfolio.html"> Portfolio </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#e-commerce" aria-expanded="false" aria-controls="e-commerce">
+          <i class="mdi mdi-shopping menu-icon"></i>
+          <span class="menu-title">E-commerce</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="e-commerce">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/invoice.html"> Invoice </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/pricing-table.html"> Pricing Table </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/orders.html"> Orders </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/emailer.html"> Email template </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/product-catalogue.html"> Product catalogue </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/project-list.html"> project list </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/kanban-board.html">
+          <i class="mdi mdi-tablet-dashboard menu-icon"></i>
+          <span class="menu-title">Kanban Board</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/email.html">
+          <i class="mdi mdi-email menu-icon"></i>
+          <span class="menu-title">E-mail</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/calendar.html">
+          <i class="mdi mdi-calendar-today menu-icon"></i>
+          <span class="menu-title">Calendar</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/todo.html">
+          <i class="mdi mdi-playlist-edit menu-icon"></i>
+          <span class="menu-title">Todo list</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/tickets.html">
+          <i class="mdi mdi-ticket menu-icon"></i>
+          <span class="menu-title">Tickets</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/chat.html">
+          <i class="mdi mdi-chat menu-icon"></i>
+          <span class="menu-title">Chat</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/apps/gallery.html">
+          <i class="mdi mdi-image-filter-frames menu-icon"></i>
+          <span class="menu-title">Gallery</span>
+        </a>
+      </li>
+      <li class="nav-item documentation-link">
+        <a class="nav-link" href="http://www.bootstrapdash.com/demo/material-dash-bootstrap/documentation/documentation.html">
+          <i class="mdi mdi-file-document-box menu-icon"></i>
+          <span class="menu-title">Documentation</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <div class="profile-actions">
+          <a href="javascript:;">Settings</a>
+          <span class="divider"></span>
+          <a href="javascript:;">Logout</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="mdc-card premium-card">
+          <div class="d-flex align-items-center">
+            <div class="mdc-card icon-card box-shadow-0">
+              <i class="mdi mdi-shield-outline"></i>
+            </div>
+            <div>
+              <p class="mt-0 mb-1 ml-2 font-weight-bold text-small">SaaS service</p>
+              <p class="mt-0 mb-0 ml-2 tx-10">Ad goes here</p>
+            </div>
+          </div>
+          <p class="tx-8 mt-3 mb-3">lorem imsum dolor sit amet</p>
+          <button class="btn btn-light w-100">Get Started</button>
+        </div>
+      </li>
+    </ul>
+  </nav>
--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -1,61 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>TILEVU</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
-  <link rel="stylesheet" href="../assets/vendors/jvectormap/jquery-jvectormap.css">
-  <!-- End plugin css for this page -->
-  <!-- Layout styles -->
-  <link rel="stylesheet" href="../assets/css/demo/style.css">
-  <!-- End layout styles -->
-  <link rel="shortcut icon" href="../assets/images/favicon.png" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TILEVU</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-material-design/css/bootstrap-material-design.min.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="../assets/vendors/jquery-bar-rating/css-stars.css">
+    <link rel="stylesheet" href="../assets/vendors/jvectormap/jquery-jvectormap.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="../assets/css/demo_1/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico" />
 
-  <script src="../assets/js/jquery-3.7.1.min.js"></script>
+    {{-- <script src="../assets/js/jquery-3.7.1.min.js"></script> --}}
 
 </head>
-<body>
-<script src="../assets/js/preloader.js"></script>
-  <div class="body-wrapper">
-    <!-- partial:partials/_sidebar.html -->
-    <x-side-bar>
-    </x-side-bar>
-    <!-- partial -->
-    <div class="main-wrapper mdc-drawer-app-content">
-      <!-- partial:partials/_navbar.html -->
-      <x-top-bar>
-      </x-top-bar>
-      <!-- partial -->
-      <div class="page-wrapper mdc-toolbar-fixed-adjust">
-        @yield('content')
-        <!-- partial:partials/_footer.html -->
-       <x-footer>
-       </x-footer>
-        <!-- partial -->
-      </div>
+<body class="sidebar-fixed">
+    <div class="container-scroller">
+
+        <x-top-bar-pro>
+        </x-top-bar-pro>
+
+        <div class="container-fluid page-body-wrapper">
+
+            <x-side-bar-pro>
+            </x-side-bar-pro>
+
+            <div class="main-panel">
+
+                @yield('content')
+
+                <x-footer-pro>
+                </x-footer-pro>
+
+            </div>
+
+        </div>
+
     </div>
-  </div>
   <!-- plugins:js -->
   <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+  <script src="../assets/vendors/bootstrap-material-design/js/bootstrap-material-design.min.js"></script>
   <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="../assets/vendors/chartjs/Chart.min.js"></script>
+  <!-- Plugin js for this page -->
+  <script src="../assets/vendors/chart.js/Chart.min.js"></script>
+  <script src="../assets/vendors/jquery-circle-progress/js/circle-progress.min.js"></script>
+  <script src="../assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
   <script src="../assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
   <script src="../assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <!-- End plugin js for this page-->
+  <script src="../assets/vendors/flot/jquery.flot.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.resize.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.categories.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.fillbetween.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.stack.js"></script>
+  <script src="../assets/vendors/flot/jquery.flot.pie.js"></script>
+  <script src="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="../assets/js/material.js"></script>
+  <script src="../assets/js/off-canvas.js"></script>
+  <script src="../assets/js/hoverable-collapse.js"></script>
   <script src="../assets/js/misc.js"></script>
+  <script src="../assets/js/settings.js"></script>
+  <script src="../assets/js/todolist.js"></script>
   <!-- endinject -->
-  <!-- Custom js for this page-->
+  <!-- Custom js for this page -->
   <script src="../assets/js/dashboard.js"></script>
-  <!-- End custom js for this page-->
+  <!-- End custom js for this page -->
 </body>
 </html>
